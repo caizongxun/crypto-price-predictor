ee3352722444f28eb8ad4fd71b85c76d44ab2740
zong <69517696+caizongxun@users.noreply.github.com>
2025-12-13 22:30:03 +0800

    Fix: Update __init__.py to import correct TFT V3 modules

--- src/__init__.py
+++ src/__init__.py
@@ -3,16 +3,18 @@ __version__ = "1.0.0"
 __author__ = "Crypto Predictor Team"
 __email__ = "support@example.com"
 
-from .data_fetcher import DataFetcher
-from .model_trainer import ModelTrainer
-from .predictor import Predictor
-from .technical_analysis import TechnicalAnalyzer
+from .data_fetcher_tft_v3 import TFTDataFetcher
+from .model_tft_v3 import TemporalFusionTransformer
 from .discord_bot import DiscordBot
+from .discord_bot_handler import DiscordBotHandler
+from .huggingface_model_manager import HuggingFaceModelManager
+from .utils import *
+from .plotting import *
 
 __all__ = [
-    'DataFetcher',
-    'ModelTrainer',
-    'Predictor',
-    'TechnicalAnalyzer',
-    'DiscordBot'
+    'TFTDataFetcher',
+    'TemporalFusionTransformer',
+    'DiscordBot',
+    'DiscordBotHandler',
+    'HuggingFaceModelManager',
 ]
