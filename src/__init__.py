7f20203e9a697de089c9844148df70209ed173a5
zong <69517696+caizongxun@users.noreply.github.com>
2025-12-13 22:31:40 +0800

    Fix: Simplify __init__.py to only import existing modules

--- src/__init__.py
+++ src/__init__.py
@@ -3,10 +3,9 @@ __version__ = "1.0.0"
 __author__ = "Crypto Predictor Team"
 __email__ = "support@example.com"
 
+# Import TFT V3 modules
 from .data_fetcher_tft_v3 import TFTDataFetcher
 from .model_tft_v3 import TemporalFusionTransformer
-from .discord_bot import DiscordBot
-from .discord_bot_handler import DiscordBotHandler
 from .huggingface_model_manager import HuggingFaceModelManager
 from .utils import *
 from .plotting import *
@@ -14,7 +13,5 @@ from .plotting import *
 __all__ = [
     'TFTDataFetcher',
     'TemporalFusionTransformer',
-    'DiscordBot',
-    'DiscordBotHandler',
     'HuggingFaceModelManager',
 ]
